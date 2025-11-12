24f461976f7236484ab44854ad82c024ea5b6486
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-11-12 15:05:36 +0000

    creat folder bu using function

---
+++ app3.js
@@ -0,0 +1,19 @@
+import chalk from "chalk";
+import fs from "fs/promises";
+import  readline from "readline-sync"
+
+
+// async function creatfolder() {
+//     try {
+//         let folderNmae=readline.question("enter folder name")
+//         await fs.mkdir( folderNmae) 
+//         // await fs.rmdir("cfi student") 
+//         console.log(("bhai folder ban gaya "));
+        
+//     } catch (error) {
+//         console.log(error);
+        
+//     }
+// }
+// creatfolder()
+

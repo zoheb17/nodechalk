79d50c5ce87d33d9134020904e080b09089923e2
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-11-13 15:37:25 +0000

    what is dotenv in node js

---
+++ app7.js
@@ -0,0 +1,15 @@
+//  what is dotenv in node.js
+
+
+// => dotenv is a node.js package the loads enviroment variable from a .env file process.env 
+
+// this is useful because:
+
+// .you dont want to secrets (api keyInSelect,Db password ) in your code 
+// you want diffderent configs dev, staging production 
+// you want clean maintainable  configuraation 
+
+
+
+// instalition
+// npm install dotenv 
